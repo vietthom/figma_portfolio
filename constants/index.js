e424592b5c48ba4af889b2f9a0e950b7191bc826
vietthom <thomle0418@gmail.com>
2023-01-07 23:53:42 -0700

    Add dummy data

--- constants/index.js
+++ constants/index.js
@@ -10,6 +10,30 @@ export const postData = [
         date: '12 Feb 2020',
         tags: ['figma', 'Icon Design'],
         summary: 'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.'
-    }
+    },
+];
+
+export const featuredWorkData = [
+    {
+        title: 'Designing Dashboards', 
+        date: '2020', 
+        tag: 'Dashboard', 
+        imgUrl: '/dashboard.png',
+        summary: 'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.',
+    }, 
+    {
+        title: 'Vibrant Portraits of 2020', 
+        date: '2020', 
+        tag: 'Illustration', 
+        imgUrl: '/vibrant.png',
+        summary: 'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.',
+    },
+    {
+        title: '36 Days of Malayalam type', 
+        date: '2018', 
+        tag: 'Typography', 
+        imgUrl: '/36days.png', 
+        summary: 'Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.',
+    },
 ];
 
