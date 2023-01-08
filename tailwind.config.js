cff648cc406b0e3aad8098b05e5caf4c139c4fa5
vietthom <thomle0418@gmail.com>
2023-01-07 23:45:13 -0700

    Install react-icons & configure tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#FF6464;',
+        'secondary': '#00A8CC',
+        'dark': '#21243D',
+        'light': '#8695A4',
+      }
+    },
   },
   plugins: [],
 }
