3dc219dd399c91e42819fcc440c409e2f5c6fd6a
vietthom <thomle0418@gmail.com>
2023-02-04 22:53:34 -0700

    Pill component

---
+++ components/Pill.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Pill = (props) => {
+  return (
+    <span className='rounded bg-[#142850] p-2 text-lg font-black text-white'>{props.date}</span>
+  )
+}
+
+export default Pill
