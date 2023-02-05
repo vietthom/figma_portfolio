a3575001f4d1beb8fb8b497e40dcba4b2ea0f0aa
vietthom <thomle0418@gmail.com>
2023-02-04 18:23:29 -0700

    Formating hero component

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,10 +1,10 @@
 import Avatar from '../public/avatar.png'
 import Image from 'next/image'
 const Hero = () => {
-  return (
+return (
     <div className="container mx-auto w-[856px] h-screen mt-[228px]">
         <div className='flex'>
-            <div className="">
+            <div>
                 <h1 className="mb-[40px] text-5xl font-bold">Hi, I am John,<br/>Creative Technologist</h1>
                 <p className="mb-[40px] text-base flex-shrink-1 w-[497px] h-[66px]">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
                 <button className="bg-primary text-white px-[20px] py-[10px]">Download Resume</button>
@@ -14,7 +14,7 @@ const Hero = () => {
             </div>
         </div>
     </div>
-  )
+)
 }
 
 export default Hero
