656badfd3aad90f93c7ad13ff6ad35d75354d6c1
vietthom <thomle0418@gmail.com>
2023-02-06 14:15:46 -0700

    Finialized blog and work page

--- pages/blog.js
+++ pages/blog.js
@@ -4,7 +4,7 @@ import { blogPosts } from '../constants'
 const blog = () => {
   return (
     <Layout>
-      <h1 className="font-bold text-5xl pt-[160px] pb-[55px]">Blog</h1>
+      <h1 className="font-bold text-5xl pt-[100px] pb-[55px]">Blog</h1>
       <ul className='divide-y divide-gray-200 pb-[30px]'>
           {blogPosts.map((blogs) => (
             <li key={blogs} className="py-4">
--- pages/works.js
+++ pages/works.js
@@ -1,7 +1,20 @@
+import { Layout, FeaturedWorkCard } from '../components'
+import { featuredWorkData } from '../constants'
 
-const works = () => {
+const works = (props) => {
   return (
-    <div>works</div>
+    <div>
+      <Layout>
+        <h1 className="text-[44px] font-black pt-6">Work</h1>
+        <ul className='divide-y divide-gray-200 pb-[30px]'>
+          {featuredWorkData.map((work) => (
+            <li key={work} className="py-1">
+              <FeaturedWorkCard {...work}/>
+            </li>
+          ))}
+        </ul>
+      </Layout>
+    </div>
   )
 }
 
